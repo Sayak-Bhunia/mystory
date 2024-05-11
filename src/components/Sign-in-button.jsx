e1a8ed889fa14d1f03c362484ac8c2ec59fef705
Abir Dutta <abirdutta390@gmail.com>
2024-05-11 07:23:37 +0530

    github icon

--- src/components/Sign-in-button.jsx
+++ src/components/Sign-in-button.jsx
@@ -1,4 +1,5 @@
 import { signIn } from "../../auth";
+import { FaGithub } from "react-icons/fa6";
 export function SignIn() {
   return (
     <form
@@ -8,10 +9,11 @@ export function SignIn() {
       }}
     >
       <button
-        className="border border-white px-2 py-1 md:px-4 md:py-2 rounded-lg hover:text-black hover:bg-white transition-all duration-300"
+        className="border border-white px-2 flex items-center justify-center gap-1 py-1 md:px-4 md:py-2 rounded-lg hover:text-black hover:bg-white transition-all duration-300"
         type="submit"
       >
         Github
+        <FaGithub className="text-lg" />
       </button>
     </form>
   );
