a9ac709b6b02180f9e446ab598921c7118461acf
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-05-12 19:54:22 +0530

    GET request implemented

--- src/app/api/confession/route.js
+++ src/app/api/confession/route.js
@@ -4,38 +4,6 @@ import User from '@/models/userModel';
 import Confession from '@/models/confessionModel';
 
 connect();
-export async function GET(req) {
-  console.log('GET request made to /api/confession');
-  try {
-    const {username} = await req.json();
-    console.log('Username:', username);
-
-    if (!username) {
-      return NextResponse.json(
-        { message: 'Please provide a username' },
-        { status: 411 }
-      );
-    }
-
-    const user = await User.findOne({ username });
-    console.log('User:', user);
-
-    if (!user) {
-      return NextResponse.json({ message: 'User not found' }, { status: 404 });
-    }
-
-    const confessions = await user.confessions;
-    console.log('Confessions:', confessions);
-
-    return NextResponse.json({ data: confessions });
-  } catch (error) {
-    console.log('Error fetching confessions:', error.message);
-    return NextResponse.json(
-      { message: 'An error occurred while fetching confessions' },
-      { status: 500 }
-    );
-  }
-}
 
 export async function POST(req) {
   console.log('POST request made to /api/confession');
