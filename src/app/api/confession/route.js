bf1400f6fc773de6546469a88d812785b25ebe24
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-05-12 20:25:21 +0530

    GET method to get all the confessions based on a username

--- src/app/api/confession/route.js
+++ src/app/api/confession/route.js
@@ -44,3 +44,35 @@ export async function POST(req) {
     );
   }
 }
+
+export async function GET(req) {
+  console.log('GET request made to /api/confession');
+  try {
+    const username = req.nextUrl.searchParams.get('username') ?? '';
+    console.log('Username:', username);
+
+    if (!username) {
+      return NextResponse.json(
+        { message: 'Please provide a username' },
+        { status: 411 }
+      );
+    }
+
+    const user = await User.findOne({ username });
+    console.log('User:', user);
+
+    if (!user) {
+      return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    }
+
+    const confessions = await Confession.find({ author: user._id });
+
+    return NextResponse.json({ data: confessions });
+  } catch (error) {
+    console.log('Error fetching confessions:', error.message);
+    return NextResponse.json(
+      { message: 'An error occurred while fetching confessions' },
+      { status: 500 }
+    );
+  }
+}
