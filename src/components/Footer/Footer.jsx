56e5244d72bbd5c394899768500579271a1e87b0
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-05-13 15:24:19 +0530

    privacy policy added 1. little changes to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,14 +13,14 @@ const Sample = () => {
     <>
       <footer className={openSans.className}>
         <div className='flex flex-col-reverse items-center justify-between space-y-8 md:space-y-0 md:grid md:grid-cols-2 py-12 border-t border-t-neutral-300 dark:border-neutral-700'>
-          <div className='flex flex-col items-center justify-between pt-16 md:pt-0'>
+          <div className='flex flex-col items-center md:items-start justify-start pt-16 md:pl-24 md:pt-0'>
             <h2 className='text-4xl md:text-6xl font-bold text-center md:text-left pb-2'>
               mystory
             </h2>
             <p className='text-gray-400 text-center'>
               A way to share stories anonymously.
             </p>
-            <div className='flex mt-4 space-x-4 items-center justify-center md:items-start md:justify-start'>
+            <div className='flex mt-4 space-x-4 items-baseline justify-center md:items-start md:justify-start'>
               <a href='https://twitter.com/girlscriptsoc' target='_blank'>
                 <FaXTwitter className='dark:text-white text-2xl' />
               </a>
@@ -34,6 +34,12 @@ const Sample = () => {
                 <FaGithub className='dark:text-white text-2xl ' />
               </a>
             </div>
+            <a href="/docs/privacy-policy" className='text-md text-neutral-500 uppercase mt-2 hover:underline hover:underline-offset-4'>
+                Privacy Policy
+            </a>
+            <a href="/docs/terms" className='text-md text-neutral-500 uppercase hover:underline hover:underline-offset-4'>
+                TERMS AND CONDITIONS
+            </a>
           </div>
           <ChangingHello />
         </div>
