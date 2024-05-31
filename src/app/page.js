0556fe8ba254880e2a1867f7430ec8f5aef55783
Aksshay88 <aksshaybala8846@gmail.com>
2024-05-31 17:31:43 +0530

    Hover-effect-of-confession added  :)

--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,7 @@ import Lenis from '@studio-freight/lenis';
 import { useEffect } from 'react';
 import { InfiniteMovingCardsDemo } from '../components/Testimonials';
 import { ThreeDCardDemo } from '../components/about/page';
+import { CardHoverEffectDemo } from '../components/importance';
 export default function Home() {
   useEffect(() => {
     const lenis = new Lenis();
@@ -27,6 +28,7 @@ export default function Home() {
         <TitleSpecial />
         <Features />
         <BuyMeACoffee />
+        <CardHoverEffectDemo />
         <ThreeDCardDemo />
         <InfiniteMovingCardsDemo />
       </Background>
